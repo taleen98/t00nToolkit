a178c35b4eeea0613b0bd99ad07c5fd60c431363
T00n <68474047+taleen98@users.noreply.github.com>
2021-05-07 07:18:22 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#########################
+#### install figlet ####
+#######################
+
+apt-get install figlet
+
+#########################
+#### install lolcat #### 
+######################## 
+
+git clone https://github.com/busyloop/lolcat.git
+
+
+##########################
+####install gobuster #### 
+######################### 
+
+git clone https://github.com/OJ/gobuster.git
