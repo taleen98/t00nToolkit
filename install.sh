70d60c0ab45ec6837adc5e41d93e8c377cb2eb5f
T00n <68474047+taleen98@users.noreply.github.com>
2021-06-06 18:29:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,10 @@ apt-get install figlet
 #### install lolcat #### 
 ######################## 
 
-git clone https://github.com/busyloop/lolcat.git
 apt install lolcat  
 
 ##########################
 ####install gobuster #### 
 ######################### 
 
-git clone https://github.com/OJ/gobuster.git
+apt install gobuster
